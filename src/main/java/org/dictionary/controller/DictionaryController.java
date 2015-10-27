4f026af678e9aa385be76ea769591ee92266a364
ChaoPang <ChaoPang229@gmail.com>
2015-10-27 01:12:42 +0100

    added guava cache to cache the word concepts

--- src/main/java/org/dictionary/controller/DictionaryController.java
+++ src/main/java/org/dictionary/controller/DictionaryController.java
@@ -3,7 +3,10 @@ package org.dictionary.controller;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.lang3.StringUtils;
 import org.dictionary.concept.WordConcept;
 import org.dictionary.crawl.impl.DictionaryWordCrawlingService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +18,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
 @Controller
 @RequestMapping("/")
 public class DictionaryController
 {
 	private final static String TEMPLATE_VIEW = "view-dictionary";
 	private final DictionaryWordCrawlingService dictionaryWordCrawlingService;
+	private final LoadingCache<String, WordConcept> cachedWordConcepts = CacheBuilder.newBuilder().maximumSize(1000)
+			.expireAfterWrite(1, TimeUnit.DAYS).build(new CacheLoader<String, WordConcept>()
+			{
+				public WordConcept load(String word)
+				{
+					return dictionaryWordCrawlingService.getWordSenses(word);
+				}
+			});
 
 	@Autowired
 	public DictionaryController(DictionaryWordCrawlingService dictionaryWordCrawlingService)
@@ -37,8 +52,12 @@ public class DictionaryController
 	@CrossOrigin
 	@RequestMapping(value = "/dictionary/{word}", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public WordConcept getWordConcept(@PathVariable("word") String wordToLookup)
+	public WordConcept getWordConcept(@PathVariable("word") String wordToLookup) throws ExecutionException
 	{
-		return dictionaryWordCrawlingService.getWordSenses(wordToLookup);
+		if (StringUtils.isNotBlank(wordToLookup))
+		{
+			return cachedWordConcepts.get(wordToLookup);
+		}
+		return WordConcept.create();
 	}
 }
